0e5c8d31a46b86737e2c16f737d3c54a2abf95db
Luca <61653175+iu2frl@users.noreply.github.com>
2024-07-08 22:44:34 +0200

    translating contest_lang

--- application/language/italian/contesting_lang.php
+++ application/language/italian/contesting_lang.php
@@ -4,20 +4,20 @@ defined('BASEPATH') OR exit('Non è permesso l\'accesso diretto allo script');
 
 $lang['contesting_page_title'] = 'Log per Contest';
 $lang['contesting_button_reset_contest_session'] = 'Azzera Sessione Contest';
-$lang['contesting_operator_callsign'] = 'Operator Callsign';
+$lang['contesting_operator_callsign'] = 'Nominativo Operatore';
 
 $lang['contesting_exchange_type'] = 'Tipologia Scambio';
 $lang['contesting_exchange_type_serial'] = 'Seriale';
-$lang['contesting_exchange_type_none'] = 'None';
-$lang['contesting_exchange_type_exchange'] = 'Exchange';
-$lang['contesting_exchange_type_gridsquare'] = 'Gridsquare';
+$lang['contesting_exchange_type_none'] = 'Nessuno';
+$lang['contesting_exchange_type_exchange'] = 'Scambio';
+$lang['contesting_exchange_type_gridsquare'] = 'Locatore';
 $lang['contesting_exchange_type_other'] = 'Altro';
-$lang['contesting_exchange_type_serial_exchange'] = 'Serial + Exchange';
-$lang['contesting_exchange_type_serial_gridsquare'] = 'Serial + Gridsquare';
-$lang['contesting_exchange_serial_s'] = 'Serial (S)';
-$lang['contesting_exchange_serial_r'] = 'Serial (R)';
-$lang['contesting_exchange_gridsquare_s'] = 'Gridsquare (S)';
-$lang['contesting_exchange_gridsquare_r'] = 'Gridsquare (R)';
+$lang['contesting_exchange_type_serial_exchange'] = 'Seriale + Scambio';
+$lang['contesting_exchange_type_serial_gridsquare'] = 'Seriale + Locatore';
+$lang['contesting_exchange_serial_s'] = 'Seriale (S)';
+$lang['contesting_exchange_serial_r'] = 'Seriale (R)';
+$lang['contesting_exchange_gridsquare_s'] = 'Locatore (S)';
+$lang['contesting_exchange_gridsquare_r'] = 'Locatore (R)';
 
 $lang['contesting_contest_name'] = 'Nome Contest';
 
@@ -27,7 +27,7 @@ $lang['contesting_btn_save_qso'] = 'Salva QSO';
 $lang['contesting_title_callsign_suggestions'] = 'Suggerimenti Nominativi';
 $lang['contesting_title_contest_logbook'] = 'Log del Contest';
 
-$lang['contesting_copy_exch_to'] = 'Copy received exchange to';
-$lang['contesting_copy_exch_hint'] = 'Exchange is only copied if it is matching rules for the selected field!';
+$lang['contesting_copy_exch_to'] = 'Copia lo scambio ricevuto a';
+$lang['contesting_copy_exch_hint'] = 'Lo scambio viene copiato solo se corrisponde alle regole per il campo selezionato!';
 
 
