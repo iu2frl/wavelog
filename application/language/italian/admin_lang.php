4968f84f31137131f9828d060727f618eb55f5ce
Luca <61653175+iu2frl@users.noreply.github.com>
2024-07-08 22:31:29 +0200

    translating admin_lang

--- application/language/italian/admin_lang.php
+++ application/language/italian/admin_lang.php
@@ -1,68 +1,67 @@
 <?php
 
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['admin_user_line1'] = 'Wavelog needs at least one user configured in order to operate.';
-$lang['admin_user_line2'] = 'Users can be assigned roles which give them different permissions, such as adding QSOs to the logbook and accessing Wavelog APIs.';
-$lang['admin_user_line3'] = 'The currently logged-in user is displayed at the upper-right of each page.';
-$lang['admin_user_line4'] = "With the password reset button, you can send a user an email containing a link to reset their password. To achieve this, ensure that the email settings in the global options are configured correctly.";
+$lang['admin_user_line1'] = 'Wavelog necessita di almeno un utente configurato per poter funzionare.';
+$lang['admin_user_line2'] = 'Agli utenti possono essere assegnati ruoli che conferiscono loro diversi permessi, come aggiungere QSOs al logbook e accedere alle API di Wavelog.';
+$lang['admin_user_line3'] = "L'utente attualmente connesso e' visualizzato in alto a destra di ogni pagina.";
+$lang['admin_user_line4'] = "Con il pulsante di reset della password, puoi inviare all'utente un'email contenente un link per reimpostare la propria password. Per fare ciò, assicurati che le impostazioni email nelle opzioni globali siano configurate correttamente.";
 
-$lang['admin_user_list'] = 'User List';
+$lang['admin_user_list'] = 'Lista Utenti';
 
-$lang['admin_user'] = 'User';
+$lang['admin_user'] = 'Utente';
 $lang['admin_email'] = 'E-mail';
-$lang['admin_type'] = 'Type';
-$lang['admin_last_seen'] = "Last seen";
-$lang['admin_last_qso'] = "Last QSO:";
-$lang['admin_no_qso_in_log'] = "No QSOs in Log";
-$lang['admin_refresh_list'] = "Refresh List";
-$lang['admin_options'] = 'Options';
+$lang['admin_type'] = 'Tipo';
+$lang['admin_last_seen'] = "Ultimo accesso";
+$lang['admin_last_qso'] = "Ultimo QSO:";
+$lang['admin_no_qso_in_log'] = "Nessun QSO nel log";
+$lang['admin_refresh_list'] = "Aggiorna Lista";
+$lang['admin_options'] = 'Opzioni';
 
 $lang['admin_station_logbooks'] = 'Logbooks';
-$lang['admin_station_locations'] = 'Locations';
+$lang['admin_station_locations'] = 'Posizioni';
 
-$lang['admin_create_user'] = 'Create user';
-$lang['admin_delete'] = 'Delete';
-$lang['admin_remove'] = "Remove";
-$lang['admin_edit'] = 'Edit';
-$lang['admin_create'] = 'Create';
-$lang['admin_update'] = 'Update';
-$lang['admin_copy'] = 'Copy';
-$lang['admin_save'] = 'Save';
-$lang['admin_close'] = 'Close';
-$lang['admin_clear'] = "Clear";
-$lang['admin_user_accounts'] = 'User Accounts';
-$lang['admin_danger'] = 'DANGER!';
-$lang['admin_experimental'] = "Experimental";
-$lang['admin_password_reset'] = "Password Reset";
+$lang['admin_create_user'] = 'Crea utente';
+$lang['admin_delete'] = 'Elimina';
+$lang['admin_remove'] = "Rimuovi";
+$lang['admin_edit'] = 'Modifica';
+$lang['admin_create'] = 'Crea';
+$lang['admin_update'] = 'Aggiorna';
+$lang['admin_copy'] = 'Copia';
+$lang['admin_save'] = 'Salva';
+$lang['admin_close'] = 'Chiudi';
+$lang['admin_clear'] = "Cancella";
+$lang['admin_user_accounts'] = 'Account Utenti';
+$lang['admin_danger'] = 'PERICOLO!';
+$lang['admin_experimental'] = "Sperimentale";
+$lang['admin_password_reset'] = "Reimposta Password";
 
-$lang['admin_email_settings_incorrect'] = "Email settings are incorrect.";
-$lang['admin_password_reset_processed'] = "Password-reset e-mail sent to user:";
-$lang['admin_confirm_pwd_reset'] = "Do you really want to send this user a password-reset link?";
-
+$lang['admin_email_settings_incorrect'] = "Le impostazioni email sono errate.";
+$lang['admin_password_reset_processed'] = "E-mail di reimpostazione della password inviata all'utente:";
+$lang['admin_confirm_pwd_reset'] = "Vuoi davvero inviare a questo utente un link per reimpostare la password?";
 
 // Contest Menu
 
-$lang['admin_contest_menu_line_1'] = 'Using the contest list, you can control which Contests are shown when logging QSOs in a contest.';
-$lang['admin_contest_menu_line_2'] = 'Active contests will be shown in the Contest Name drop-down, while inactive contests will be hidden and cannot be selected.';
-$lang['admin_contest_menu_name'] = 'Name';
-$lang['admin_contest_menu_adif'] = 'ADIF Name';
-$lang['admin_contest_menu_active'] = 'Active';
-$lang['admin_contest_menu_n_active'] = 'Not Active';
-$lang['admin_contest_menu_activate'] = 'Activate';
-$lang['admin_contest_menu_deactivate'] = 'Deactivate';
+$lang['admin_contest_menu_line_1'] = 'Utilizzando la lista dei contest, puoi controllare quali contest vengono mostrati quando registri QSOs in un contest.';
+$lang['admin_contest_menu_line_2'] = 'I contest attivi saranno mostrati nel menu a tendina Nome Contest, mentre i contest inattivi saranno nascosti e non potranno essere selezionati.';
+$lang['admin_contest_menu_name'] = 'Nome';
+$lang['admin_contest_menu_adif'] = 'Nome ADIF';
+$lang['admin_contest_menu_active'] = 'Attivo';
+$lang['admin_contest_menu_n_active'] = 'Non Attivo';
+$lang['admin_contest_menu_activate'] = 'Attiva';
+$lang['admin_contest_menu_deactivate'] = 'Disattiva';
 
-$lang['admin_contest_add_contest'] = 'Add a Contest';
-$lang["admin_contest_create"] = "Create";
-$lang['admin_contest_all_active'] = 'Activate All';
-$lang['admin_contest_all_deactive'] = 'Deactivate All';
+$lang['admin_contest_add_contest'] = 'Aggiungi un Contest';
+$lang["admin_contest_create"] = "Crea";
+$lang['admin_contest_all_active'] = 'Attiva Tutti';
+$lang['admin_contest_all_deactive'] = 'Disattiva Tutti';
 
-$lang['admin_contest_name_adif'] = 'Contest ADIF Name';
-$lang['admin_contest_name_of_contest'] = 'Name of the Contest';
-$lang['admin_contest_name_of_adif'] = 'Name of Contest in ADIF-specification';
-$lang['admin_contest_edit_active_hint'] = 'Set to active if to be listed in Contest-list';
-$lang['admin_contest_edit_update_contest'] = 'Update Contest';
-$lang['admin_contest_deletion_warning'] = 'Warning! Are you sure you want to delete the following contest: ';
-$lang['admin_contest_active_all_warning'] = 'Warning! Are you sure you want to activate all contests?';
-$lang['admin_contest_deactive_all_warning'] = 'Warning! Are you sure you want to deactivate all contests?';
-
+$lang['admin_contest_name_adif'] = 'Nome ADIF del Contest';
+$lang['admin_contest_name_of_contest'] = 'Nome del Contest';
+$lang['admin_contest_name_of_adif'] = 'Nome del Contest nella specifica ADIF';
+$lang['admin_contest_edit_active_hint'] = 'Imposta su attivo se deve essere elencato nella lista dei Contest';
+$lang['admin_contest_edit_update_contest'] = 'Aggiorna Contest';
+$lang['admin_contest_deletion_warning'] = 'Attenzione! Sei sicuro di voler eliminare il seguente contest: ';
+$lang['admin_contest_active_all_warning'] = 'Attenzione! Sei sicuro di voler attivare tutti i contest?';
+$lang['admin_contest_deactive_all_warning'] = 'Attenzione! Sei sicuro di voler disattivare tutti i contest?';
