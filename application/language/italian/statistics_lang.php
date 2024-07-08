4e83857f88e8ec09a0e6ab854d17fbbcfcae64cd
Luca <61653175+iu2frl@users.noreply.github.com>
2024-07-08 23:02:28 +0200

    translating statistics_lang

--- application/language/italian/statistics_lang.php
+++ application/language/italian/statistics_lang.php
@@ -2,37 +2,37 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['statistics_statistics'] = 'Statistics';
+$lang['statistics_statistics'] = 'Statistiche';
 
-$lang['statistics_explore_the_logbook'] = 'Explore the logbook.';
+$lang['statistics_explore_the_logbook'] = 'Esplora il logbook.';
 
-$lang['statistics_years'] = 'Years';
-$lang['statistics_modes'] = 'Mode';
-$lang['statistics_bands'] = 'Bands';
-$lang['statistics_qsos'] = 'QSOs';
-$lang['statistics_unique_callsigns'] = 'Unique callsigns';
+$lang['statistics_years'] = 'Anni';
+$lang['statistics_modes'] = 'Modi';
+$lang['statistics_bands'] = 'Band';
+$lang['statistics_qsos'] = 'QSO';
+$lang['statistics_unique_callsigns'] = 'Indicativi unici';
 
-$lang['statistics_total'] = 'Total';
+$lang['statistics_total'] = 'Totale';
 
-$lang['statistics_year'] = 'Year';
+$lang['statistics_year'] = 'Anno';
 
-$lang['statistics_number_of_qso_worked_each_year'] = "Number of QSOs worked each year";
-$lang['statistics_number_of_qso_worked'] = "# of QSO's worked";
+$lang['statistics_number_of_qso_worked_each_year'] = "Numero di QSO effettuati ogni anno";
+$lang['statistics_number_of_qso_worked'] = "Numero di QSO effettuati";
 
 /*
 *
-* Distances
+* Distanze
 *
 */
 
-$lang['statistics_distances_worked'] = "Distances Worked";
-$lang['statistics_distances_part1_contacts_were_plotted_furthest'] = "contacts were plotted.<br /> Your furthest contact was with";
-$lang['statistics_distances_part2_contacts_were_plotted_furthest'] = "in gridsquare";
-$lang['statistics_distances_part3_contacts_were_plotted_furthest'] = "the distance was";
-$lang['statistics_distances_number_of_qsos'] = "Number of QSOs";
-$lang['statistics_distances_callsigns_worked'] = "Callsign(s) worked (max 5 shown)";
-$lang['statistics_distances_qsos_with'] = "QSOs with";
-$lang['statistics_distances_and_band'] = "and band";
+$lang['statistics_distances_worked'] = "Distanze Percorse";
+$lang['statistics_distances_part1_contacts_were_plotted_furthest'] = "contatti sono stati tracciati.<br /> Il tuo contatto più lontano è stato con";
+$lang['statistics_distances_part2_contacts_were_plotted_furthest'] = "nella griglia quadrata";
+$lang['statistics_distances_part3_contacts_were_plotted_furthest'] = "la distanza era di";
+$lang['statistics_distances_number_of_qsos'] = "Numero di QSO";
+$lang['statistics_distances_callsigns_worked'] = "Indicativi lavorati (mostrati fino a 5)";
+$lang['statistics_distances_qsos_with'] = "QSO con";
+$lang['statistics_distances_and_band'] = "e banda";
 
 /*
 *
@@ -40,44 +40,43 @@ $lang['statistics_distances_and_band'] = "and band";
 *
 */
 
-$lang['statistics_timeline'] = "Timeline";
+$lang['statistics_timeline'] = "Cronologia";
 
 /*
 *
-* Days with QSO
+* Giorni con QSO
 *
 */
 
-$lang['statistics_days_with_qso'] = "Number of days with QSOs each year";
-$lang['statistics_days_with_qso_short'] = "Days with QSOs";
-$lang['statistics_dwq_longest_streak_in_log'] = "Longest streak with QSOs in the log";
-$lang['statistics_dwq_longest_streak_in_log_hint'] = "A maximum of the 10 longest streaks are shown!";
-$lang['statistics_dwq_streak_continuous_days'] = "Streak (continuous days with QSOs)";
-$lang['statistics_dwq_current_streak_in_log'] = "Current streak with QSOs in the log";
-$lang['statistics_dwq_current_streak_continuous_days'] = "Current streak (continuous days with QSOs)";
-$lang['statistics_dwq_make_qso_to_extend_streak'] = "If you make a QSO today, you can continue to extend your streak... or else your current streak will be broken!";
-$lang['statistics_dwq_no_current_streak'] = "No current streak found!";
+$lang['statistics_days_with_qso'] = "Numero di giorni con QSO ogni anno";
+$lang['statistics_days_with_qso_short'] = "Giorni con QSO";
+$lang['statistics_dwq_longest_streak_in_log'] = "Streak più lunga con QSO nel log";
+$lang['statistics_dwq_longest_streak_in_log_hint'] = "Sono mostrati al massimo le 10 streak più lunghe!";
+$lang['statistics_dwq_streak_continuous_days'] = "Streak (giorni consecutivi con QSO)";
+$lang['statistics_dwq_current_streak_in_log'] = "Streak attuale con QSO nel log";
+$lang['statistics_dwq_current_streak_continuous_days'] = "Streak attuale (giorni consecutivi con QSO)";
+$lang['statistics_dwq_make_qso_to_extend_streak'] = "Se fai un QSO oggi, puoi continuare a estendere la tua streak... altrimenti la tua streak attuale verrà interrotta!";
+$lang['statistics_dwq_no_current_streak'] = "Nessuna streak attuale trovata!";
 
 /*
 *
-* Accumulated Stats
+* Statistiche Accumulate
 *
 */
 
-$lang['statistics_accumulated_worked_dxcc'] = "Accumulated Number of DXCCs worked";
-$lang['statistics_accumulated_worked_states'] = "Accumulated Number of States worked";
-$lang['statistics_accumulated_worked_iota'] = "Accumulated Number of IOTAs worked";
-$lang['statistics_accumulated_worked_cqzone'] = "Accumulated Number of CQ Zones worked";
+$lang['statistics_accumulated_worked_dxcc'] = "Numero accumulato di DXCC lavorati";
+$lang['statistics_accumulated_worked_states'] = "Numero accumulato di stati lavorati";
+$lang['statistics_accumulated_worked_iota'] = "Numero accumulato di IOTA lavorati";
+$lang['statistics_accumulated_worked_cqzone'] = "Numero accumulato di zone CQ lavorate";
 
 /*
 *
 * Timeplotter
 *
 */
- 
-$lang['statistics_timeplotter_description'] = "The Timeplotter is used to analyze your logbook and find out at what times you worked certain CQ zones or DXCC countries on a selected band.";
-$lang['statistics_timeplotter_contacts_plotted'] = "contacts were plotted"; /* sentence starts with counter - 'XXXX contacts were plotted' */
-$lang['statistics_timeplotter_chart_header'] = "Time Distribution";
-$lang['statistics_timeplotter_number_of_qsos'] = "Number of QSOs";
-$lang['statistics_timeplotter_callsigns_worked'] = "Callsign(s) worked (max 5)";
 
+$lang['statistics_timeplotter_description'] = "Il Timeplotter è utilizzato per analizzare il tuo logbook e scoprire a che ora hai lavorato determinate zone CQ o paesi DXCC su una banda selezionata.";
+$lang['statistics_timeplotter_contacts_plotted'] = "contatti sono stati tracciati"; /* la frase inizia con il contatore - 'XXXX contatti sono stati tracciati' */
+$lang['statistics_timeplotter_chart_header'] = "Distribuzione del tempo";
+$lang['statistics_timeplotter_number_of_qsos'] = "Numero di QSO";
+$lang['statistics_timeplotter_callsigns_worked'] = "Indicativi lavorati (mostrati fino a 5)";
