b2ee36a74c7ccd2fc6729a484aabc05d9f5b6633
Luca <61653175+iu2frl@users.noreply.github.com>
2024-07-08 22:24:27 +0200

    translating account_lang

--- application/language/italian/account_lang.php
+++ application/language/italian/account_lang.php
@@ -2,135 +2,137 @@
 
 defined('BASEPATH') OR exit('Non è permesso l\'accesso diretto allo script');
 
-$lang['account_logbook_fields'] = 'Campi Logbook';
+$lang['account_logbook_fields'] = 'Campi del Logbook';
 $lang['account_column1_text'] = 'Scegli colonna 1';
 $lang['account_column2_text'] = 'Scegli colonna 2';
 $lang['account_column3_text'] = 'Scegli colonna 3';
 $lang['account_column4_text'] = 'Scegli colonna 4';
 $lang['account_column5_text'] = 'Scegli colonna 5 (solo per il logbook)';
 
-$lang['account_create_user_account'] = 'Create User Account';
-$lang['account_edit_account'] = 'Edit Account';
+$lang['account_create_user_account'] = 'Crea Account Utente';
+$lang['account_edit_account'] = 'Modifica Account';
 
-$lang['account_account_information'] = "Account";
-$lang['account_user'] = "User";
-$lang['account_word_edited'] = "edited";
-$lang['account_username'] = 'Username';
-$lang['account_email_address'] = 'Email Address';
+$lang['account_account_information'] = 'Informazioni Account';
+$lang['account_user'] = 'Utente';
+$lang['account_word_edited'] = 'modificato';
+$lang['account_username'] = 'Nome utente';
+$lang['account_email_address'] = 'Indirizzo email';
 $lang['account_password'] = 'Password';
 
-$lang['account_roles'] = 'Roles';
-$lang['account_user_role'] = 'User Role';
-$lang['account_word_admin'] = 'Admin';
+$lang['account_roles'] = 'Ruoli';
+$lang['account_user_role'] = 'Ruolo Utente';
+$lang['account_word_admin'] = 'Amministratore';
 
-$lang['account_theme'] = 'Theme';
-$lang['account_stylesheet'] = 'Stylesheet';
+$lang['account_theme'] = 'Tema';
+$lang['account_stylesheet'] = 'Foglio di stile';
 
-$lang['account_personal_information'] = "Personal";
-$lang['account_first_name'] = 'First Name';
-$lang['account_last_name'] = 'Last Name';
+$lang['account_personal_information'] = 'Informazioni Personali';
+$lang['account_first_name'] = 'Nome';
+$lang['account_last_name'] = 'Cognome';
 
-$lang['account_hamradio_information'] = "Ham Radio";
-$lang['account_callsign'] = 'Callsign';
-$lang['account_gridsquare'] = 'Gridsquare';
+$lang['account_hamradio_information'] = 'Informazioni Radioamatore';
+$lang['account_callsign'] = 'Nominativo';
+$lang['account_gridsquare'] = 'Locatore';
 
-$lang['account_wavelog_preferences'] = 'Wavelog Preferences';
-$lang['account_timezone'] = 'Timezone';
-$lang['account_date_format'] = 'Date Format';
-$lang['account_log_end_time'] = 'Log End Times for QSOs Separately';
-$lang['account_log_end_time_hint'] = 'Choose yes here if you want to log QSO start and end times separately. If set to \'No\' the end time will be the same as start time.';
-$lang['account_quicklog_feature'] = "Quicklog Field";
-$lang['account_quicklog_feature_hint'] = "With this feature, you can log callsigns using the search field in the header.";
-$lang['account_quicklog_enter'] = "Quicklog - Action on press Enter";
-$lang['account_quicklog_enter_hint'] = "What action should be performed when Enter is pressed in the quicklog field?";
-$lang['account_quicklog_enter_log'] = "Log Callsign";
-$lang['account_quicklog_enter_search'] = "Search Callsign";
-$lang['account_measurement_preferences'] = 'Measurement preference';
-$lang['account_select_how_you_would_like_dates_shown_when_logged_into_your_account'] = 'Select how you would like dates shown when logged into your account.';
-$lang['account_choose_which_unit_distances_will_be_shown_in'] = 'Choose which unit distances will be shown in';
-$lang['account_wavelog_language'] = 'Wavelog Language';
-$lang['account_choose_wavelog_language'] = 'Choose Wavelog language.';
+$lang['account_wavelog_preferences'] = 'Preferenze Wavelog';
+$lang['account_timezone'] = 'Fuso orario';
+$lang['account_date_format'] = 'Formato Data';
+$lang['account_log_end_time'] = 'Registra separatamente i tempi di fine dei QSO';
+$lang['account_log_end_time_hint'] = 'Scegli sì se desideri registrare separatamente i tempi di inizio e fine dei QSO. Se impostato su "No", il tempo di fine sarà lo stesso di quello di inizio.';
+$lang['account_quicklog_feature'] = 'Campo Quicklog';
+$lang['account_quicklog_feature_hint'] = 'Con questa funzione, puoi registrare i nominativi utilizzando il campo di ricerca nell\'intestazione.';
+$lang['account_quicklog_enter'] = 'Quicklog - Azione alla pressione del tasto Invio';
+$lang['account_quicklog_enter_hint'] = 'Quale azione deve essere eseguita quando viene premuto Invio nel campo quicklog?';
+$lang['account_quicklog_enter_log'] = 'Registra Nominativo';
+$lang['account_quicklog_enter_search'] = 'Cerca Nominativo';
+$lang['account_measurement_preferences'] = 'Preferenza di Misurazione';
+$lang['account_select_how_you_would_like_dates_shown_when_logged_into_your_account'] = 'Seleziona come desideri che le date vengano visualizzate quando accedi al tuo account.';
+$lang['account_choose_which_unit_distances_will_be_shown_in'] = 'Scegli in quale unità verranno mostrate le distanze';
+$lang['account_wavelog_language'] = 'Lingua di Wavelog';
+$lang['account_choose_wavelog_language'] = 'Scegli la lingua di Wavelog.';
 
-$lang['account_main_menu'] = 'Menu Options';
-$lang['account_show_notes_in_the_main_menu'] = 'Show notes in the main menu.';
+$lang['account_main_menu'] = 'Opzioni del Menu';
+$lang['account_show_notes_in_the_main_menu'] = 'Mostra note nel menu principale.';
 
-$lang['account_gridsquare_and_location_autocomplete'] = 'Gridsquare and Location Autocomplete';
-$lang['account_location_auto_lookup'] = 'Location auto lookup.';
-$lang['account_if_set_gridsquare_is_fetched_based_on_location_name'] = 'If set, gridsquare is fetched based on location name.';
-$lang['account_sota_auto_lookup_gridsquare_and_name_for_summit'] = 'SOTA auto lookup gridsquare and name for summit.';
-$lang['account_wwff_auto_lookup_gridsquare_and_name_for_reference'] = 'WWFF auto lookup gridsquare and name for reference.';
-$lang['account_pota_auto_lookup_gridsquare_and_name_for_park'] = 'POTA auto lookup gridsquare and name for park.';
-$lang['account_if_set_name_and_gridsquare_is_fetched_from_the_api_and_filled_in_location_and_locator'] = 'If set, name and gridsquare is fetched from the API and filled in location and locator.';
+$lang['account_gridsquare_and_location_autocomplete'] = 'Autocompletamento Locatore e Località';
+$lang['account_location_auto_lookup'] = 'Ricerca automatica della località.';
+$lang['account_if_set_gridsquare_is_fetched_based_on_location_name'] = 'Se impostato, il locatore viene recuperato in base al nome della località.';
+$lang['account_sota_auto_lookup_gridsquare_and_name_for_summit'] = 'Ricerca automatica SOTA del locatore e del nome per la vetta.';
+$lang['account_wwff_auto_lookup_gridsquare_and_name_for_reference'] = 'Ricerca automatica WWFF del locatore e del nome per il riferimento.';
+$lang['account_pota_auto_lookup_gridsquare_and_name_for_park'] = 'Ricerca automatica POTA del locatore e del nome per il parco.';
+$lang['account_if_set_name_and_gridsquare_is_fetched_from_the_api_and_filled_in_location_and_locator'] = 'Se impostato, nome e locatore vengono recuperati dall\'API e compilati in località e locator.';
 
-$lang['account_previous_qsl_type'] = 'Previous QSL Type';
-$lang['account_select_the_type_of_qsl_to_show_in_the_previous_qsos_section'] = 'Select the type of QSL to show in the previous QSOs section.';
+$lang['account_previous_qsl_type'] = 'Tipo di QSL Precedente';
+$lang['account_select_the_type_of_qsl_to_show_in_the_previous_qsos_section'] = 'Seleziona il tipo di QSL da mostrare nella sezione dei QSO precedenti.';
 
-$lang['account_qrzcom_hamqthcom_images'] = 'qrz.com/hamqth.com Images';
-$lang['account_show_profile_picture_of_qso_partner_from_qrzcom_hamqthcom_profile_in_the_log_qso_section'] = 'Show profile picture of QSO partner from qrz.com/hamqth.com profile in the log QSO section.';
-$lang['account_please_set_your_qrzcom_hamqthcom_credentials_in_the_general_config_file'] = 'Please set your qrz.com/hamqth.com credentials in the general config file.';
+$lang['account_qrzcom_hamqthcom_images'] = 'Immagini da qrz.com/hamqth.com';
+$lang['account_show_profile_picture_of_qso_partner_from_qrzcom_hamqthcom_profile_in_the_log_qso_section'] = 'Mostra l\'immagine del profilo del partner QSO dal profilo qrz.com/hamqth.com nella sezione del registro QSO.';
+$lang['account_please_set_your_qrzcom_hamqthcom_credentials_in_the_general_config_file'] = 'Si prega di impostare le credenziali qrz.com/hamqth.com nel file di configurazione generale.';
 
-$lang['account_amsat_status_upload'] = 'AMSAT Status Upload';
-$lang['account_upload_status_of_sat_qsos_to'] = 'Upload status of SAT QSOs to';
+$lang['account_amsat_status_upload'] = 'Stato caricamento AMSAT';
+$lang['account_upload_status_of_sat_qsos_to'] = 'Carica lo stato dei QSO SAT';
 
 $lang['account_logbook_of_the_world'] = 'Logbook of the World';
-$lang['account_logbook_of_the_world_lotw_username'] = 'Logbook of The World (LoTW) Username';
-$lang['account_logbook_of_the_world_lotw_password'] = 'Logbook of The World (LoTW) Password';
+$lang['account_logbook_of_the_world_lotw_username'] = 'Nome utente di Logbook of The World (LoTW)';
+$lang['account_logbook_of_the_world_lotw_password'] = 'Password di Logbook of The World (LoTW)';
 
 $lang['account_clublog'] = 'Club Log';
-$lang['account_clublog_email_callsign'] = 'Club Log Email/Callsign';
-$lang['account_clublog_password'] = 'Club Log Password';
-$lang['account_the_email_or_callsign_you_use_to_login_to_club_log'] = 'The Email or Callsign you use to login to Club Log';
+$lang['account_clublog_email_callsign'] = 'Email/Nominativo Club Log';
+$lang['account_clublog_password'] = 'Password Club Log';
+$lang['account_the_email_or_callsign_you_use_to_login_to_club_log'] = 'L\'email o il nominativo che usi per accedere a Club Log';
 
 $lang['account_eqsl'] = 'eQSL';
-$lang['account_eqslcc_username'] = 'eQSL.cc Username';
-$lang['account_eqslcc_password'] = 'eQSL.cc Password';
+$lang['account_eqslcc_username'] = 'Nome utente eQSL.cc';
+$lang['account_eqslcc_password'] = 'Password eQSL.cc';
 
-$lang['account_save_account_changes'] = "Save Account";
-$lang['account_create_account'] = 'Create Account';
+$lang['account_save_account_changes'] = 'Salva Account';
+$lang['account_create_account'] = 'Crea Account';
 
-$lang['account_delete_user_account'] = 'Delete User Account';
-$lang['account_are_you_sure_you_want_to_delete_the_user_account'] = 'Are you sure you want to delete the user account';
-$lang['account_yes_delete_this_user'] = 'Yes, delete this user';
-$lang['account_no_do_not_delete_this_user'] = 'No, do not delete this user';
+$lang['account_delete_user_account'] = 'Elimina Account Utente';
+$lang['account_are_you_sure_you_want_to_delete_the_user_account'] = 'Sei sicuro di voler eliminare questo account utente?';
+$lang['account_yes_delete_this_user'] = 'Sì, elimina questo utente';
+$lang['account_no_do_not_delete_this_user'] = 'No, non eliminare questo utente';
 
-$lang['account_forgot_password'] = 'Forgot Password?';
-$lang['account_you_can_reset_your_password_here'] = 'You can reset your password here.';
-$lang['account_reset_password'] = 'Reset Password';
-$lang['account_the_email_field_is_required'] = 'The Email field is required';
-$lang['account_confirm_password'] = 'Confirm Password';
+$lang['account_forgot_password'] = 'Hai dimenticato la password?';
+$lang['account_you_can_reset_your_password_here'] = 'Puoi reimpostare la tua password qui.';
+$lang['account_reset_password'] = 'Reimposta Password';
+$lang['account_the_email_field_is_required'] = 'Il campo Email è obbligatorio';
+$lang['account_confirm_password'] = 'Conferma Password';
 
-$lang['account_forgot_your_password'] = 'Forgot your password?';
+$lang['account_forgot_your_password'] = 'Hai dimenticato la tua password?';
 
-$lang['account_login'] = 'Login';
+$lang['account_login'] = 'Accedi';
 
-$lang['account_mastodon'] = 'Mastodonserver';
-$lang['account_user_mastodon'] = 'URL of Mastodonserver';
-$lang['account_user_mastodon_hint'] = "Main URL of your Mastodon server, e.g. <a href='https://radiosocial.de/' target='_blank'>https://radiosocial.de";
+$lang['account_mastodon'] = 'Server Mastodon';
+$lang['account_user_mastodon'] = 'URL del server Mastodon';
+$lang['account_user_mastodon_hint'] = 'URL principale del tuo server Mastodon, ad esempio <a href=\'https://radiosocial.de/\' target=\'_blank\'>https://radiosocial.de</a>';
 
-$lang['account_default_band_settings'] = 'Settings for Default Band and Confirmation';
-$lang['account_gridmap_default_band'] = 'Default Band';
-$lang['account_qsl_settings'] = 'Default QSL-Methods';
+$lang['account_default_band_settings'] = 'Impostazioni della Banda Predefinita e QSL';
+$lang['account_gridmap_default_band'] = 'Banda Predefinita';
+$lang['account_qsl_settings'] = 'Metodi QSL Predefiniti';
 
 $lang['account_winkeyer'] = 'Winkeyer';
-$lang['account_winkeyer_hint'] = "Winkeyer support in Wavelog is very experimental read the wiki first at <a href='https://github.com/wavelog/wavelog/wiki/Winkey' target='_blank'>https://github.com/wavelog/wavelog/wiki/Winkey</a> before enabling.";
-$lang['account_winkeyer_enabled'] = "Winkeyer Features Enabled";
+$lang['account_winkeyer_hint'] = 'Il supporto Winkeyer in Wavelog è molto sperimentale, leggi prima il wiki su <a href=\'https://github.com/wavelog/wavelog/wiki/Winkey\' target=\'_blank\'>https://github.com/wavelog/wavelog/wiki/Winkey</a> prima di abilitarlo.';
+$lang['account_winkeyer_enabled'] = 'Funzionalità Winkeyer Abilitate';
 
-$lang['account_map_params'] = "Map Settings";
-$lang['account_map_qso_by_default'] = "QSO (by default)";
-$lang['account_map_qso_confirm'] = "QSO (confirmed)";
-$lang['account_map_qso_confirm_same_qso'] = "(If 'No', displayed as ".$lang['account_map_qso_by_default'].")";
+$lang['account_map_params'] = 'Impostazioni Mappa';
+$lang['account_map_qso_by_default'] = 'QSO (di default)';
+$lang['account_map_qso_confirm'] = 'QSO (confermati)';
+$lang['account_map_qso_confirm_same_qso'] = '(Se "No", visualizzato come ' . $lang['account_map_qso_by_default'] . ')';
 
-$lang['account_general_information'] = "General Information";
-$lang['account_qso_logging_options'] = "QSO Logging Options";
-$lang['account_third_party_services'] = "Third Party Services";
-$lang['account_default_values'] = "Default Values";
-$lang['account_miscellaneous'] = "Miscellaneous";
+$lang['account_general_information'] = 'Informazioni Generali';
+$lang['account_qso_logging_options'] = 'Opzioni di Registrazione QSO';
+$lang['account_third_party_services'] = 'Servizi di Terze Parti';
+$lang['account_default
 
-$lang['account_hamsat'] = "Hams.at";
-$lang['account_hamsat_private_feed_key'] = "Private Feed Key";
-$lang['account_hamsat_hint'] = "See your profile at <a href='https://hams.at/users/settings' target='_blank'>https://hams.at/users/settings</a>.";
-$lang['account_hamsat_workable_only'] = "Show Workable Passes Only";
-$lang['account_hamsat_workable_only_hint'] = "If enabled shows only workable passes based on the gridsquare set in your hams.at account. Requires private feed key to be set.";
+_values'] = 'Valori Predefiniti';
+$lang['account_miscellaneous'] = 'Varie';
 
-$lang['account_references_show'] = "Show Reference Fields on QSO Tab";
-$lang['account_show_references_on_1st_tab'] = "The enabled items will be shown on the QSO tab rather than the General tab.";
+$lang['account_hamsat'] = 'Hams.at';
+$lang['account_hamsat_private_feed_key'] = 'Chiave del Feed Privato';
+$lang['account_hamsat_hint'] = 'Vedi il tuo profilo su <a href=\'https://hams.at/users/settings\' target=\'_blank\'>https://hams.at/users/settings</a>.';
+$lang['account_hamsat_workable_only'] = 'Mostra Solo Passaggi Lavorabili';
+$lang['account_hamsat_workable_only_hint'] = 'Se abilitato, mostra solo i passaggi lavorabili in base al locatore impostata nel tuo account hams.at. Richiede che la chiave del feed privato sia impostata.';
+
+$lang['account_references_show'] = 'Mostra Campi di Riferimento sulla Scheda QSO';
+$lang['account_show_references_on_1st_tab'] = 'Gli elementi abilitati verranno mostrati sulla scheda QSO anziché sulla scheda Generale.';
